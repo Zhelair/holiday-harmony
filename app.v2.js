e507fea1d4b54992c352347561ec6bc86f5aa512
Zhelair <nik.sales.737@gmail.com>
2025-12-24 15:53:29 +0200

    Update app.v2.js

--- app.v2.js
+++ app.v2.js
@@ -18,7 +18,7 @@ if (!window.supabase || !window.supabase.createClient) {
 // ✅ PASTE REAL VALUES (KEEP QUOTES!)
 
 const SUPABASE_URL = "https://ubthnjsdxuhjyjnrxube.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidGhuanNkeHVoanlqbnJ4dWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1Njc1OTIsImV4cCI6MjA4MjE0MzU5Mn0.zOUuQErKK2sOhIbmG2OVbwBkuUe3TfrEEGBlH7-dE_g";
+const SUPABASE_ANON_PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidGhuanNkeHVoanlqbnJ4dWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1Njc1OTIsImV4cCI6MjA4MjE0MzU5Mn0.zOUuQErKK2sOhIbmG2OVbwBkuUe3TfrEEGBlH7-dE_g";
 
 if (!SUPABASE_URL || !SUPABASE_ANON_PUBLIC_KEY) {
   debug("❌ Missing Supabase keys in app.js");
