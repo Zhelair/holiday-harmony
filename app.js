117d2b36bbbd20ad91af5fe1498640deeeda14be
Zhelair <nik.sales.737@gmail.com>
2025-12-24 14:00:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
 // ✅ Paste your Supabase URL + anon key here:
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_PUBLIC_KEY";
+const SUPABASE_URL = "https://ubthnjsdxuhjyjnrxube.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidGhuanNkeHVoanlqbnJ4dWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1Njc1OTIsImV4cCI6MjA4MjE0MzU5Mn0.zOUuQErKK2sOhIbmG2OVbwBkuUe3TfrEEGBlH7-dE_g";
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
