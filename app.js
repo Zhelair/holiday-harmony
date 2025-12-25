ec1821e447e3acbb54f79c5a6bb11e825aac37c7
Zhelair <nik.sales.737@gmail.com>
2025-12-25 18:33:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ if (!window.supabase || !window.supabase.createClient) {
 }
 
 // ✅ PASTE YOUR REAL VALUES (ONLY HERE)
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL";
+const SUPABASE_URL = "https://ubthnjsdxuhjyjnrxube.supabase.co";
 const SUPABASE_ANON_PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVidGhuanNkeHVoanlqbnJ4dWJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY1Njc1OTIsImV4cCI6MjA4MjE0MzU5Mn0.zOUuQErKK2sOhIbmG2OVbwBkuUe3TfrEEGBlH7-dE_g";
 
 const supa = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_PUBLIC_KEY);
