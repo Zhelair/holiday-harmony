592c89031e69733679607c95771661f762d48034
Zhelair <nik.sales.737@gmail.com>
2025-12-25 14:32:32 +0200

    Update app.js

--- app.js
+++ app.js
@@ -592,7 +592,7 @@ choreBtn?.addEventListener("click", () => {
 function msToMmSs(ms) {
   const s = Math.max(0, Math.floor(ms / 1000));
   const mm = String(Math.floor(s / 60)).padStart(2,"0");
-  const ss = String(s % 60)).padStart(2,"0");
+  const ss = String(s % 60).padStart(2,"0");
   return `${mm}:${ss}`;
 }
 
